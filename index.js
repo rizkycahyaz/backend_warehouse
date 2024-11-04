4eb9da54c0212d0f856ffc116f37874735c815e4
albi nur <albi27076@gmail.com>
2024-11-04 09:38:37 +0700

    memperbaiki query, menambah route

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const authRoutes = require("./routes/authRoutes");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const locationRoutes = require("./routes/locationRoutes");
-const itemRoutes = require("./routes/itemRoutes");
-const errorHandler = require("./middlewares/errorHandler");
-const { addLocationValidator } = require("./validators/locationValidator");
-const path = require("path");
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const locationRoutes = require('./routes/locationRoutes');
+const itemRoutes = require('./routes/itemRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+const { addLocationValidator } = require('./validators/locationValidator');
+const path = require('path');
 const port = 3000;
 
 const app = express();
@@ -14,17 +14,19 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/static", express.static(path.join(__dirname, "public/images")));
+app.use('/static', express.static(path.join(__dirname, 'public/images')));
 
 // Routes
 // Routes
-app.use("/api/auth", authRoutes); // Pastikan authRoutes tidak membutuhkan authMiddleware
+app.use('/api/auth', authRoutes); // Pastikan authRoutes tidak membutuhkan authMiddleware
 // Halaman utama tidak butuh middleware otentikasi
-app.use("/api/items", itemRoutes); 
+app.use('/api/items', itemRoutes);
 // Hanya halaman admin yang butuh middleware otentikasi
-app.use("/api/admin/items", itemRoutes);
-app.use("/api/locations", locationRoutes);
+app.use('/api/admin/items', itemRoutes);
 
+app.use('/api/locations', locationRoutes);
+
+app.use('/api/items/search', itemRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
